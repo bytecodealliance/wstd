00066be3e8bfce440b7d7eee3da50253d5d612d4
Pat Hickey <p.hickey@f5.com>
2025-10-28 12:24:02 -0700

    readmes

---
+++ aws/README.md
@@ -0,0 +1,75 @@
+
+# wstd-aws: wstd support for the AWS Rust SDK
+
+This crate provides support for using the AWS Rust SDK for the `wasm32-wasip2`
+target using the [`wstd`] crate.
+
+In many wasi settings, its necessary or desirable to use the wasi-http
+interface to make http requests. Wasi-http interfaces provide an http
+implementation, including the sockets layer and TLS, outside of the user's
+component. `wstd` provides user-friendly async Rust interfaces to all of the
+standardized wasi interfaces, including wasi-http.
+
+The AWS Rust SDK, by default, depends on `tokio`, `hyper`, and either `rustls`
+or `s2n_tls`, and makes http requests over sockets (which can be provided as
+wasi-sockets). Those dependencies may not work correctly under `wasm32-wasip2`,
+and if they do, they will not use the wasi-http interfaces. To avoid using
+http over sockets, make sure to set the `default-features = false` setting
+when depending on any `aws-*` crates in your project.
+
+To configure `wstd`'s wasi-http client for the AWS Rust SDK, provide
+`wstd_aws::sleep_impl()` and `wstd_aws::http_client()` to your
+[`aws_config::ConfigLoader`]:
+
+```
+    let config = aws_config::defaults(BehaviorVersion::latest())
+        .sleep_impl(wstd_aws::sleep_impl())
+        .http_client(wstd_aws::http_client())
+        ...;
+```
+
+[`wstd`]: https://docs.rs/wstd/latest/wstd
+[`aws_config::ConfigLoader`]: https://docs.rs/aws-config/1.8.8/aws_config/struct.ConfigLoader.html
+
+## Example
+
+An example s3 client is provided as a wasi cli command. It accepts command
+line arguments with the subcommand `list` to list a bucket's contents, and
+`get <key>` to get an object from a bucket and write it to the filesystem.
+
+This example *must be compiled in release mode* - in debug mode, the aws
+sdk's generated code will overflow the maximum permitted wasm locals in
+a single function.
+
+Compile it with:
+
+```sh
+cargo build -p wstd-aws --target wasm32-wasip2 --release --examples
+```
+
+When running this example, you will need AWS credentials provided in environment
+variables.
+
+Run it with:
+```sh
+wasmtime run -Shttp \
+    --env AWS_ACCESS_KEY_ID \
+    --env AWS_SECRET_ACCESS_KEY \
+    --env AWS_SESSION_TOKEN \
+    --dir .::. \
+    target/wasm22-wasip2/release/examples/s3.wasm
+```
+
+or alternatively run it with:
+```sh
+cargo run --target wasm32-wasip2 -p wstd-aws --example s3
+```
+
+which uses the wasmtime cli, as above, via configiration found in this
+workspace's `.cargo/config`.
+
+By default, this script accesses the `wstd-example-bucket` in `us-west-2`.
+To change the bucket or region, use the `--bucket` and `--region` cli
+flags before the subcommand.
+
+
--- aws/examples/s3.rs
+++ aws/examples/s3.rs
@@ -1,3 +1,44 @@
+//! Example s3 client running on `wstd` via `wstd_aws`
+//!
+//! This example is a wasi cli command. It accepts command line arguments
+//! with the subcommand `list` to list a bucket's contents, and `get <key>`
+//! to get an object from a bucket and write it to the filesystem.
+//!
+//! This example *must be compiled in release mode* - in debug mode, the aws
+//! sdk's generated code will overflow the maximum permitted wasm locals in
+//! a single function.
+//!
+//! Compile it with:
+//!
+//! ```sh
+//! cargo build -p wstd-aws --target wasm32-wasip2 --release --examples
+//! ```
+//!
+//! When running this example, you will need AWS credentials provided in environment
+//! variables.
+//!
+//! Run it with:
+//! ```sh
+//! wasmtime run -Shttp \
+//!     --env AWS_ACCESS_KEY_ID \
+//!     --env AWS_SECRET_ACCESS_KEY \
+//!     --env AWS_SESSION_TOKEN \
+//!     --dir .::. \
+//!     target/wasm22-wasip2/release/examples/s3.wasm
+//! ```
+//!
+//! or alternatively run it with:
+//! ```sh
+//! cargo run --target wasm32-wasip2 -p wstd-aws --example s3
+//! ```
+//!
+//! which uses the wasmtime cli, as above, via configiration found in this
+//! workspace's `.cargo/config`.
+//!
+//! By default, this script accesses the `wstd-example-bucket` in `us-west-2`.
+//! To change the bucket or region, use the `--bucket` and `--region` cli
+//! flags before the subcommand.
+
 use anyhow::Result;
 use clap::{Parser, Subcommand};
 
@@ -7,12 +48,13 @@ use aws_sdk_s3::Client;
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
 struct Opts {
-    /// The AWS Region.
+    /// The AWS Region. Defaults to us-west-2 if not provided.
     #[arg(short, long)]
-    region: String,
-    /// The name of the bucket.
+    region: Option<String>,
+    /// The name of the bucket. Defaults to wstd-example-bucket if not
+    /// provided.
     #[arg(short, long)]
-    bucket: String,
+    bucket: Option<String>,
 
     #[command(subcommand)]
     command: Option<Command>,
@@ -31,8 +73,17 @@ enum Command {
 #[wstd::main]
 async fn main() -> Result<()> {
     let opts = Opts::parse();
+    let region = opts
+        .region
+        .clone()
+        .unwrap_or_else(|| "us-west-2".to_owned());
+    let bucket = opts
+        .bucket
+        .clone()
+        .unwrap_or_else(|| "wstd-example-bucket".to_owned());
+
     let config = aws_config::defaults(BehaviorVersion::latest())
-        .region(Region::new(opts.region.clone()))
+        .region(Region::new(region))
         .sleep_impl(wstd_aws::sleep_impl())
         .http_client(wstd_aws::http_client())
         .load()
@@ -42,11 +93,11 @@ async fn main() -> Result<()> {
 
     match opts.command.as_ref().unwrap_or(&Command::List) {
         Command::List => {
-            let output = list(&opts, &client).await?;
+            let output = list(&bucket, &client).await?;
             print!("{}", output);
         }
         Command::Get { key, out } => {
-            let contents = get(&opts, &client, key).await?;
+            let contents = get(&bucket, &client, key).await?;
             let output: &str = if let Some(out) = out {
                 out.as_str()
             } else {
@@ -58,10 +109,10 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-async fn list(opts: &Opts, client: &Client) -> Result<String> {
+async fn list(bucket: &str, client: &Client) -> Result<String> {
     let mut listing = client
         .list_objects_v2()
-        .bucket(opts.bucket.clone())
+        .bucket(bucket.to_owned())
         .into_paginator()
         .send();
 
@@ -86,10 +137,10 @@ async fn list(opts: &Opts, client: &Client) -> Result<String> {
     Ok(output)
 }
 
-async fn get(opts: &Opts, client: &Client, key: &str) -> Result<Vec<u8>> {
+async fn get(bucket: &str, client: &Client, key: &str) -> Result<Vec<u8>> {
     let object = client
         .get_object()
-        .bucket(opts.bucket.clone())
+        .bucket(bucket.to_owned())
         .key(key)
         .send()
         .await?;
