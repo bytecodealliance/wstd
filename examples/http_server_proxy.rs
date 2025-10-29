e653a1f72e821aaa5f54f64167c2b2049df65a5e
Tomas Olvecky <tomas.olvecky@gmail.com>
2025-10-29 20:52:57 +0100

    Finish the streaming proxy example

--- examples/http_server_proxy.rs
+++ examples/http_server_proxy.rs
@@ -3,7 +3,7 @@
 // wasmtime serve -Scli -Shttp --env TARGET_URL=https://example.com/ target/wasm32-wasip2/debug/examples/http_server_proxy.wasm
 // Test with `curl --no-buffer -v 127.0.0.1:8080/proxy/`
 
-use wstd::http::body::{Body, Bytes};
+use wstd::http::body::Body;
 use wstd::http::{Client, Error, Request, Response, StatusCode, Uri};
 
 const PROXY_PREFIX: &str = "/proxy/";
@@ -53,10 +53,12 @@ async fn proxy(server_req: Request<Body>, target_url: Uri) -> Result<Response<Bo
             .expect("no errors could be in ResponseBuilder")
             .append(key, value.clone());
     }
-    // FIXME: Convert UnsyncBoxBody<Bytes, Error> to Response<Body>
-    server_resp
+    let resp_body = server_resp
         .body(client_resp.into_body().into_boxed_body())
-        .map_err(Error::from)
+        .map_err(Error::from)?;
+    let (resp_parts, resp_body) = resp_body.into_parts();
+    let resp_body = Body::from_http_body(resp_body);
+    Ok(Response::from_parts(resp_parts, resp_body))
 }
 
 fn http_not_found(_request: Request<Body>) -> Response<Body> {
