1231184343a8bb6922279c56064f54f1d58efd6f
Yosh <github@yosh.is>
2026-04-08 17:43:23 +0200

    fix cfg

--- src/sys/mod.rs
+++ src/sys/mod.rs
@@ -1,10 +1,9 @@
 cfg_if::cfg_if! {
-    if #[cfg(any(feature = "wasip3", all(target_os = "wasi", target_env = "p3")))] {
-        mod p3;
-        use p3 as backend;
-    } else {
+    if #[cfg(all(target_os = "wasi", target_env = "p2"))] {
         mod p2;
         use p2 as backend;
+    } else {
+        compile_error!("unsupported target: wstd only compiles on `wasm32-wasip2`");
     }
 }
 
