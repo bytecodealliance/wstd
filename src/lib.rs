729189edb2f4df871a02d82f6cdc7470eab762d2
Adam Bratschi-Kaye <a.bratschi-kaye@f5.com>
2026-09-16 14:00:10 +0000

    only include module under wasi

--- src/lib.rs
+++ src/lib.rs
@@ -65,6 +65,7 @@ pub mod io;
 pub mod iter;
 #[cfg(all(target_os = "wasi", target_env = "p2"))]
 pub mod net;
+#[cfg(target_os = "wasi")]
 pub mod rand;
 #[cfg(all(target_os = "wasi", target_env = "p2"))]
 pub mod runtime;
